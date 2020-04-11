3bfca12d553a27cf3a7e91ca5abeed1b996f33d1
mariesto <sitinjakamendo@gmail.com>
2020-04-12 00:53:19 +0700

    initial

---
+++ src/main/java/com/mariesto/exploration/springbootrestfulwithjpa/SpringBootRestfulWithJpaApplication.java
@@ -0,0 +1,13 @@
+package com.mariesto.exploration.springbootrestfulwithjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulWithJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulWithJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mariesto/exploration/springbootrestfulwithjpa/SpringBootRestfulWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariesto.exploration.springbootrestfulwithjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulWithJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
